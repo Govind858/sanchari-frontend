f9e44d3e417f74a4aa91ce430c1b0a5409a3c053
anazks <anazksunil2@gmail.com>
2025-02-13 19:32:12 +0530

    addded login

--- src/app/Components/MainContent/MainContent.jsx
+++ src/app/Components/MainContent/MainContent.jsx
@@ -1,24 +1,29 @@
 "use client";
 import React from 'react'
 import './MainContent.css'
-
+import Login from '../LoginComponent/Login';
+import TripDetails from '../TripDetails/TripDetails';
+import Map from '../Map/Map';
 function MainContent() {
   return (
     <div className='mainContent'>
       
         <div className='tripForm'>
-          <form action="" className='tripContainer'>
+          {/* <form action="" className='tripContainer'>
             <div><input id='tripLoc' type="text"  placeholder='Pickup Location'/></div>
             <div> <input id='tripLoc' type="text" placeholder='Dropoff Location'/></div>
             <div><button id='seePrice'>see pirce</button></div>
-          </form>
+          </form> */}
+          {/* <Login/> */}
+          <TripDetails/>
         </div>
         <div className='tagLine'>
-          <div className='tagLineContent'>
-            <h1 id='heading'>Sanchari</h1>
-            <h1 >Go wherever.</h1>
-            <h1 >whenever.</h1>
-          </div>
+          {/* <div className='tagLineContent'>
+            <h1 id='heading'>𝙎𝙖𝙣𝙘𝙝𝙖𝙧𝙞</h1>
+            <h1>𝑮𝒐 𝑾𝒉𝒆𝒏 𝒆𝒗𝒆𝒓</h1>
+            <h1>𝑾𝒉𝒆𝒏 𝒆𝒗𝒆𝒓</h1> */}
+          {/* </div> */}
+        <Map/>
         </div>
     </div>
   )
--- src/app/page.js
+++ src/app/page.js
@@ -4,14 +4,13 @@ import Header from "./Components/Header/Header";
 
 export default function Home() {
   const backgroundStyle = {
-    backgroundImage: "url('/back-ground-img.jpg')", // Ensure the image is inside the 'public' folder
-    backgroundSize: "cover",
+    backgroundSize: "cover", // Or "contain" depending on your need
     backgroundPosition: "center",
     height: "100vh", // Full screen height
     width: "100vw",  // Full screen width
-    backgroundSize: "contain"
+    background: "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 33%, rgba(0,212,255,1) 100%)"
   };
-
+  
   return (
     <div style={backgroundStyle}>
       <Header />
