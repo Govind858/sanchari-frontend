a8171c97fa241cf63971ea4cf8935de9aef6699c
anazks <anazksunil2@gmail.com>
2025-02-14 10:38:56 +0530

    added ui model

--- src/app/Components/LoginComponent/Login.jsx
+++ src/app/Components/LoginComponent/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 import { FcGoogle } from "react-icons/fc";
 import { FaFacebook } from "react-icons/fa";
 import './Login.css'
